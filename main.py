885923b906017b0addb534d0eb9ace5f39bceaa0
axentraSupport <axentrababamarka@gmail.com>
2026-01-12 22:20:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN = os.getenv("8444472609:AAGuvazsikXGTb262Hw9MBI15eZflh-dw-o")
+ADMIN_ID = int(os.getenv("8561815348"))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Destek aktif, mesajını yazabilirsin.")
+
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await context.bot.send_message(
+        chat_id=ADMIN_ID,
+        text=f"📩 {user.first_name}\n🆔 {user.id}\n\n{update.message.text}"
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message))
+
+app.run_polling()
